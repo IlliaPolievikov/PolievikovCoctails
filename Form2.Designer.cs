7ca707148d901a090b4d60fab1dc5fa3c0ed4b6f
Ilia Polevikov <Ilia Polevikov>
2016-03-30 16:57:39 +0300

    first commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(164, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Azure;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(6, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Показать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Azure;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(499, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 186);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Обновить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(300, 199);
+            this.listView2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(195, 205);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(499, 199);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 205);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 76);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(280, 328);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // listView1
+            // 
+            this.listView1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.listView1.Location = new System.Drawing.Point(300, 11);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(195, 186);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(631, 418);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.Text = "Управление коктейлеми";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+
+            conn_string.Server = "localhost";
+            conn_string.UserID = "root";
+            conn_string.Password = "";
+            conn_string.Database = "cocktails";
+
+            mcon = new MySqlConnection(conn_string.ToString());
+        }
+        MySqlConnection mcon;
+        DataSet ds = new DataSet();
+       
+
+        //Обновить
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                // dataGridView2.Rows.Clear();
+                mcon.Close();
+                string reciept = "";
+
+                //reciept  содержит название коктейля
+                reciept = listView1.SelectedItems[0].Text;
+                MySqlDataAdapter mda = new MySqlDataAdapter("select ingridients from cocktails.drinks where name='" + reciept +"'", mcon);
+                mcon.Open();
+                mda.Fill(ds, "drinks");
+                //dataGridView2.DataSource = ds.Tables["drinks"]; //должно выполняться в фоне
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Поиск
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                mcon.Close();
+                MySqlDataAdapter mda = new MySqlDataAdapter("select * from cocktails.drinks", mcon);
+                mcon.Open();
+                mda.Fill(ds, "drinks");
+                dataGridView1.DataSource = ds.Tables["drinks"]; //должно выполняться в фоне
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Добавить
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mcon.Close();
+            MySqlDataAdapter mda = new MySqlDataAdapter("select * from cocktails.drinks ", mcon);
+            mcon.Open();
+            DataTable dt = new DataTable();
+            mda.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                ListViewItem listitem = new ListViewItem(dr["name"].ToString());
+                listitem.SubItems.Add(dr["name"].ToString());
+                listView1.Items.Add(listitem);
+                
+            }
+        }
+        //Добавить
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mcon.Close();
+            string reciept = "";
+            
+            //reciept  содержит название коктейля
+            reciept = listView1.SelectedItems[0].Text;
+            MySqlDataAdapter mda = new MySqlDataAdapter("select * from cocktails.drinks", mcon);
+            mcon.Open();
+            DataTable dt = new DataTable();
+            mda.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                ListViewItem listitem = new ListViewItem(dr["ingridients"].ToString());
+                listitem.SubItems.Add(dr["ingridients"].ToString());
+                listView2.Items.Add(listitem);
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
